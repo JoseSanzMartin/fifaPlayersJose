18b7d1dc21d26a482393433486f56cd0d65e0592
JoséSanz <josesanzmartin@hotmail.es>
2023-04-06 13:47:25 +0200

    modifying unneeded lines of code in the interceptor

--- src/app/core/interceptor/error.interceptor.ts
+++ src/app/core/interceptor/error.interceptor.ts
@@ -1,33 +1,3 @@
-/*import {
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { Observable, catchError, throwError } from "rxjs";
-
-@Injectable()
-export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    console.log("ENTRÓ!!2");
-    return next.handle(req).pipe(
-      catchError((err) => {
-        console.log(err);
-        if ([401, 403, 404].indexOf(err.status) !== -1) {
-        }
-        this.router.navigate(["/404"]);
-        return throwError(err);
-      })
-    );
-  }
-}*/
-
 import {
   HTTP_INTERCEPTORS,
   HttpErrorResponse,
@@ -50,21 +20,13 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    console.log("Interceptor ejecutado");
     return next.handle(req).pipe(
       catchError((error) => {
         if (error instanceof HttpErrorResponse && error.status == 404) {
           this.router.navigateByUrl("/not-found", { replaceUrl: true });
-
           return EMPTY;
         } else return throwError(error);
       })
     );
   }
 }
-
-export const HttpErrorInterceptorProvider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: HttpErrorInterceptor,
-  multi: true,
-};
